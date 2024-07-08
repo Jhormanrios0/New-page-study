9c85a6d4a139fd215b566458ef4f404dd27dfa09
jhorman <78864961+Jhormanrios0@users.noreply.github.com>
2024-07-08 09:55:47 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Blue_gov: "#3D6BB1",
+      },
+    },
+  },
+  plugins: [],
+};
